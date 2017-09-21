d64d72bca1b0f27f454bb4770254ad5253692bdf
Rodolfo Ribeiro Barreto <rodolfobarretoweb@gmail.com>
2017-09-21 18:02:13 -0300

    Create pagination system

--- src/actions/api.js
+++ src/actions/api.js
@@ -3,9 +3,11 @@ import { get as getApi } from '../utils/api';
 export const SEARCH_API    = 'SEARCH_API';
 export const GET_BY_ID_API = 'GET_BY_ID_API';
 
-export function search(params) {
+export function search(query, startIndex = 0) {
   return dispatch => {
-    return getApi(`/volumes?q=${params}`).then((response) => {
+    return getApi(
+      `/volumes?q=${query}&startIndex=${startIndex}&maxResults=10`
+    ).then((response) => {
       const payload = response.data;
 
       dispatch({ type : SEARCH_API, payload });
--- src/langs/search.js
+++ src/langs/search.js
@@ -2,6 +2,11 @@ export const search = {
   title : 'Search books',
   noResults : 'No results to display. Try type something...',
 
+  paginationButtons : {
+    previous : 'Previous page',
+    next : 'Next page'
+  },
+
   fields : {
     query : 'Type your search',
     searchButton : 'Research',
